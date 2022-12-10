0720c3d59e3530a7cb71721626984ac1f2978908
Alex <alexandersmith223@gmail.com>
2022-12-10 15:50:40 -0500

    Require first_name and last_name on user
    - Update sign up page for first_name and last_name
    - Update validations on User model
    - Add null: false constraint to first_name and last_name columns on
      users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   has_many :stars
   has_many :starred_posts, class_name: 'Post', foreign_key: :post_id, through: :stars, source: :post
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
