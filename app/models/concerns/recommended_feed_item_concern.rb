76a0f02eca02af343429626551983727960784d2
gangelo <web.gma@gmail.com>
2022-12-20 17:00:44 -0500

    Remove erroneous logging

--- app/models/concerns/recommended_feed_item_concern.rb
+++ app/models/concerns/recommended_feed_item_concern.rb
@@ -5,7 +5,6 @@ module RecommendedFeedItemConcern
   # feed item is already recommended; otherwise, it will recommend
   # (add) the feed item.
   def toggle_feed_item_recommended(id)
-    Rails.logger.warn("xyzzy: toggle_feed_item_recommended")
     return unrecommend_feed_item(id) if feed_item_recommended?(id)
 
     recommend_feed_item(id)
@@ -16,14 +15,12 @@ module RecommendedFeedItemConcern
   end
 
   def recommend_feed_item(id)
-    Rails.logger.warn("xyzzy: recommend_feed_item")
     feed_item = Feed.find(id)
     feeds << feed_item
     feed_item
   end
 
   def unrecommend_feed_item(id)
-    Rails.logger.warn("xyzzy: unrecommend_feed_item")
     feed_item = Feed.find(id)
     feeds.delete(feed_item)
     feed_item
