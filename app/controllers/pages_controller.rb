93396d68a57946029e9ab14bc5674593b1c1f1e9
Amit Anand <amit@firstthought.io>
2023-05-06 00:06:16 -0400

    simple api pull to fetch top news

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+
+    def home
+        require 'net/http'
+        require 'json'
+
+        # Fetch the top story IDs from the Hacker News API
+        uri = URI('https://hacker-news.firebaseio.com/v0/topstories.json')
+        top_story_ids = JSON.parse(Net::HTTP.get(uri)).take(10)
+
+        # Fetch details for each top story
+        @top_stories = top_story_ids.map do |story_id|
+          story_uri = URI("https://hacker-news.firebaseio.com/v0/item/#{story_id}.json")
+          JSON.parse(Net::HTTP.get(story_uri))
+        end
+      end
 end
