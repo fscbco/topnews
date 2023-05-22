99f958e40b35c70cc24e8070a1e864ccf48353e7
Omar Riaz <omar@Omars-MBP.attlocal.net>
2023-05-22 12:41:49 -0500

    removing unecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   }
   root to: redirect(path: '/news')
   get '/news', to: 'pages#news'
-  # resources :news do
-  #   member do
-  #     patch :flag
-  #   end
-  # end
   patch '/news/:news_id/flag/:user_name', to: 'news#flag', as: 'flag_news'
   devise_scope :user do
     root 'devise/sessions#new', as: :unauthenticated_root
