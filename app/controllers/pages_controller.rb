a1fe307219260e3ab8c1ef2954030652e98886ae
Brandon <bclai27@gmail.com>
2023-06-09 17:54:40 -0400

    Update to correctly format time

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,7 @@ class PagesController < ApplicationController
         end
         ids.each do |id|
             story_json = call_api("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
-            story = Story.new(title: story_json["title"], author: story_json["by"], url: story_json["url"], api_id: story_json["id"], object_type: story_json["type"], time_posted: story_json["time"])
+            story = Story.new(title: story_json["title"], author: story_json["by"], url: story_json["url"], api_id: story_json["id"], object_type: story_json["type"], time_posted: Time.at(story_json["time"]))
             stories.append(story)
         end
         return stories
