1af4bcccf590675ee8a1118f1a982701a9681129
Brandon <bclai27@gmail.com>
2023-06-09 16:07:38 -0400

    Add logic for getting top stories from API and create separate pages for Top Stories and Saved Stories
    -Create method for consuming third party API to get the top stories
    -Display general story information on the Top Stories HTML page
    -Created new pages for both Top Stories (the new home page) and Saved Stories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,24 @@
 class PagesController < ApplicationController
     before_action :authenticate_user!
     
-    def home
+    def top_stories
+        top_story_ids = top_stories_ids = call_api("https://hacker-news.firebaseio.com/v0/topstories.json")
+        @stories = get_stories_for_ids(top_story_ids, 20)
+    end
+    
+    def saved_stories
+    end
+
+    def get_stories_for_ids(ids, limit=nil)
+        stories = []
+        if limit != nil
+            ids = ids.take(limit)
+        end
+        ids.each do |id|
+            story_json = call_api("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
+            story = Story.new(title: story_json["title"], author: story_json["by"], url: story_json["url"], api_id: story_json["id"], object_type: story_json["type"], time_posted: story_json["time"])
+            stories.append(story)
+        end
+        return stories
     end
 end
