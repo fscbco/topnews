b02f3628356854c26a1ad1963c5df040a85e6182
Brandon <bclai27@gmail.com>
2023-06-09 16:03:22 -0400

    Add utility method for making API call
    Create a general method for calling the third party API using 'rest-client' gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  require 'rest-client'
+  require 'json'
   protect_from_forgery with: :exception
+
+  def call_api(url)
+    response = RestClient.get(url)
+    object = JSON.parse(response)
+    return object
+  end
 end
