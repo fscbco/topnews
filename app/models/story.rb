a573f63aa550b4b5b78bb0c67eb031b5c0d0521b
Brandon <bclai27@gmail.com>
2023-06-09 21:00:18 -0400

    Update Story and User model to correctly reference `StorySave` class

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ApplicationRecord
-    has_many :story_saves, dependent: :destroy
+    has_many :story_saves, dependent: :destroy, class_name: "StorySave"
     has_many :users, through: :story_saves
     validates :api_id, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :story_saves, dependent: :destroy
+  has_many :story_saves, dependent: :destroy, class_name: "StorySave"
   has_many :stories, through: :story_saves
 end
