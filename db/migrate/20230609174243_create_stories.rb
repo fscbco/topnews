3052f3114e97df73e7ea1bac190cb03e7a8cf826
Brandon <bclai27@gmail.com>
2023-06-09 20:15:42 -0400

    Update Story object migration to add required fields and ensure unique api_id
    -Make title field required in database
    -Make url field required in database
    -Make api_id field required in database and add constraint to make sure it is unique so that multiple rows cannot reference the same story

--- db/migrate/20230609174243_create_stories.rb
+++ db/migrate/20230609174243_create_stories.rb
@@ -1,14 +1,16 @@
 class CreateStories < ActiveRecord::Migration[7.0]
   def change
     create_table :stories do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :author
-      t.string :url
-      t.integer :api_id
+      t.string :url, null: false
+      t.integer :api_id, null: false, unique: true
       t.string :object_type
       t.timestamp :time_posted
 
       t.timestamps
     end
+
+    add_index :stories, :api_id, unique: true
   end
 end
