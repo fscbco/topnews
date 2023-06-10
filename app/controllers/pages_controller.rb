0116c2335e9e438848a0446952499277c48a458c
Brandon <bclai27@gmail.com>
2023-06-09 20:21:12 -0400

    Add functionality for saving stories
    Added a "Save Story" button in the view that POSTs to a method that stores the saved story data in the database

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,22 +3,40 @@ class PagesController < ApplicationController
     
     def top_stories
         top_story_ids = top_stories_ids = call_api("https://hacker-news.firebaseio.com/v0/topstories.json")
-        @stories = get_stories_for_ids(top_story_ids, 50)
+        @stories = get_stories_from_ids(top_story_ids, 50)
     end
     
     def saved_stories
     end
 
-    def get_stories_for_ids(ids, limit=nil)
+    def save_story
+        if !Story.exists?(api_id: params[:api_id])
+            story = get_story_from_id(params[:api_id])
+            story.save
+        else
+            story = Story.find_by(api_id: params[:api_id])
+        end
+        if !StorySave.exists?(user_id: current_user.id, story_id: story.id, api_id: story.api_id)
+            StorySave.create(user_id: current_user.id, story_id: story.id, api_id: story.api_id)
+        end
+        redirect_to root_path
+    end
+
+    def get_stories_from_ids(ids, limit=nil)
         stories = []
         if limit != nil
             ids = ids.take(limit)
         end
         ids.each do |id|
-            story_json = call_api("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
-            story = Story.new(title: story_json["title"], author: story_json["by"], url: story_json["url"], api_id: story_json["id"], object_type: story_json["type"], time_posted: Time.at(story_json["time"]))
+            story = get_story_from_id(id)
             stories.append(story)
         end
         return stories
+    end
+
+    def get_story_from_id(id)
+        story_json = call_api("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
+        story = Story.new(title: story_json["title"], author: story_json["by"], url: story_json["url"], api_id: story_json["id"], object_type: story_json["type"], time_posted: Time.at(story_json["time"]))
+        return story
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#top_stories'
   get 'saved-stories', to: 'pages#saved_stories'
+  post 'save_story', to: 'pages#save_story'
 end
