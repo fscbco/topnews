c9b792e80ba1407ed3c40db7d785604f7d357f28
saurav jain <saurav.jain@alphasights.com>
2023-06-19 22:30:55 -0400

    add newlines

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-
   private
-
     def sign_up_params
       params.require(:user).permit(:email, :password, :first_name, :last_name)
     end
