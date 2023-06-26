53d778776520cceb9b4d8114403fe2952ca67779
Peter Alcock <me@peteralcock.com>
2023-06-26 07:34:42 -0400

    update routes for user story starring

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :stories
+  get  '/starred_stories'    => 'starred_stories#index'
+  post    '/starred_stories'    => 'starred_stories#create',     as: 'starred_stories_path'
   root to: 'pages#home'
+
 end
