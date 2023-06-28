05d211250457426d1f1c64d29eb7321d98e06045
Peter Alcock <peteralcock@users.noreply.github.com>
2023-06-27 20:38:47 -0400

    Update Story Spec

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -4,3 +4,10 @@ RSpec.describe Story, type: :model do
   it { should have_many(:starred_stories).dependent(:destroy) }
   # Add more tests for Story validations, associations, and methods as needed
 end
+
+RSpec.describe StarredStory, type: :model do
+  it { should belong_to(:user) }
+  it { should belong_to(:story) }
+  # Add more tests for StarredStory validations, associations, and methods as needed
+end
+
