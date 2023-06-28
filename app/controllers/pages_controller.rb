382819500fcdf82c63646b7c196dc68df06c25b4
Lawrence Chong <chonglawr@gmail.com>
2023-06-27 22:58:06 -0400

    Display user stories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
     def home
         top_story_ids = TopStory.latest.external_ids.first(10)
-        stories = Story.from_hacker_news.where(external_id: top_story_ids)
+        stories = Story.includes(:users).from_hacker_news.where(external_id: top_story_ids)
         order = top_story_ids.each_with_index.to_h
         @hacker_news_stories = stories.sort_by{ |story| order[story.external_id] }
+
+        @user_stories = current_user.stories.from_hacker_news.includes(:users) if current_user
     end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,7 @@
 class Story < ApplicationRecord
     belongs_to :source
+    has_many :user_stories
+    has_many :users, through: :user_stories
 
     scope :from_hacker_news, -> { joins(:source).where(source: { name: Source::HACKER_NEWS }) }
 end
