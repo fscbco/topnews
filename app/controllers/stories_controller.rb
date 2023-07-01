fd59f0a5c66fdc6846cb74ca73df39071b33028a
Bryan <bryanwong92@gmail.com>
2023-07-01 17:50:06 -0400

    Adding logic to story controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,6 +6,43 @@ class StoriesController < ApplicationController
  end
 
  def show
-   @story = HackerNewsApi.get_story_details(params[:id])
+  @story = Story.find_by(id: params[:id])
+
+  if @story
+    render :show
+  else
+    redirect_to stories_path, alert: "Story not found"
+  end
+end
+
+def flag
+ @story_id = params[:id]
+ story_details = HackerNewsApi.get_story_details(@story_id)
+
+ if story_details.present?
+   @story = Story.find_or_initialize_by(story_id: @story_id)
+
+   if @story.new_record?
+     @story.title = story_details['title']
+     @story.url = story_details['url']
+     @story.flagged = true
+     @story.flagged_by = current_user.id
+     @story.user_id = current_user.id
+     @story.save
+   end
+
+   if @story.persisted?
+     redirect_to story_path(@story)
+   else
+     puts @story.errors.full_messages
+     redirect_to stories_path, alert: "Story not flagged"
+   end
+ else
+   redirect_to stories_path, alert: "Story not found"
  end
+end
+
+def flagged
+  @flagged_stories = Story.joins(:flags).distinct
+end
 end
