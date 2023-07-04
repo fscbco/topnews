3ae50801956d0ce0a346b74a6f9c6d00c98600ae
Josh Joll <40342278+joshjoll@users.noreply.github.com>
2023-07-04 10:55:01 -0500

    small view improvements

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,8 @@
 class Story < ApplicationRecord
     has_many :stars
     has_many :users, through: :stars
+
+    def pretty_time
+        "#{time&.strftime('%d %b, %Y')} <br>#{time&.strftime('%l:%M')}"
+    end
 end
