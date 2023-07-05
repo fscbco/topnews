fc03532b8f4f7085ce43a0be84e123b2657d3ebe
Peter Alcock <peteralcock@users.noreply.github.com>
2023-07-05 16:50:55 -0400

    Update hacker_news_service.rb

--- app/services/hacker_news_service.rb
+++ app/services/hacker_news_service.rb
@@ -12,4 +12,12 @@ class HackerNewsService
       JSON.parse(response)
     end
   end
+
+  def save_stories
+      stories = self.top_stories
+      stories.each do |story|
+      Story.find_or_create_by(:title => story["title"], :id => story['id'], :user_id => current_user.id, :url => story['url'], :author => story['by'])
+    end
+  end
+  
 end
