39dddb6c564f48ac639ca3367ce3b193160b7419
Peter Alcock <peteralcock@users.noreply.github.com>
2023-07-05 16:47:39 -0400

    Fix pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,8 @@
 class PagesController < ApplicationController
+  def home
+    @stories = HackerNewsService.new.top_stories
+    @stories.each do |story|
+      Story.find_or_create_by(:title => story["title"], :id => story['id'], :user_id => current_user.id, :url => story['url'], :author => story['by'])
+    end
+  end
 end
