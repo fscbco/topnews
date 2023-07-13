f37fb7e2f33ebbfb113686c4493cf29d375ce99c
Pat Needham <patrick.h.needham@gmail.com>
2023-07-13 10:07:48 -0400

    new schema for a user to star a hacker news story

---
+++ db/migrate/20230713140439_create_stories.rb
@@ -0,0 +1,12 @@
+class CreateStories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :stories do |t|
+      t.integer :hn_id
+      t.string :title
+      t.string :url
+      t.string :hn_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230713140609_create_starred_stories.rb
@@ -0,0 +1,10 @@
+class CreateStarredStories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :starred_stories do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :story, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
