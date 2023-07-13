44f9f9c08cceeefc438039ad5ff05e6baf643876
Pat Needham <patrick.h.needham@gmail.com>
2023-07-13 10:51:46 -0400

    fixed bug for starring stories multiple times and added strong params

--- app/controllers/starred_stories_controller.rb
+++ app/controllers/starred_stories_controller.rb
@@ -3,27 +3,36 @@ class StarredStoriesController < ApplicationController
     # Find an existing story by its Hacker News ID, or create a new one if it doesn't exist.
     # This is done using the find_or_create_by method, which first tries to find a record
     # with the given attributes, and if it can't find one, creates a new record.
-    story = Story.find_or_create_by(hn_id: params[:story_id]) do |story|
+    story = Story.find_or_create_by(hn_id: story_params[:hn_id]) do |story|
       # If a new story is being created, we set its attributes here
-      story.title = params[:title]
-      story.url = params[:url]
-      story.hn_url = params[:hn_url]
+      story.title = story_params[:title]
+      story.url = story_params[:url]
+      story.hn_url = story_params[:hn_url]
     end
 
-    # Create a new StarredStory record, which represents the fact that a user has
-    # starred a story. This record associates the user with the story.
-    starred_story = StarredStory.new(user_id: params[:user_id], story: story)
-
-    # Try to save the StarredStory record to the database. If this is successful,
-    # show a flash message to let the user know. If there was an error saving the record,
-    # show a different flash message.
-    if starred_story.save
-      flash[:notice] = 'Story starred successfully.'
+    # Find an existing StarredStory record for this user and story
+    if StarredStory.find_by(user_id: starred_story_params[:user_id], story: story)
+      flash[:notice] = 'Story already starred.'
     else
-      flash[:alert] = 'There was an error starring the story.'
+      # or create a new one if it doesn't exist.
+      starred_story = StarredStory.create(user_id: starred_story_params[:user_id], story: story)
+      if starred_story.save
+        flash[:notice] = 'Story starred successfully.'
+      else
+        flash[:alert] = 'There was an error starring the story.'
+      end
     end
 
-    # After attempting to star the story, redirect the user back to the homepage.
     redirect_to root_path
+  end
+
+  private
+
+  def starred_story_params
+    params.permit(:user_id)
+  end
+
+  def story_params
+    params.permit(:title, :url, :hn_url, :hn_id)
   end
 end
