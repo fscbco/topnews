afcb158874b9d32c746cdde0fac2d88ac1f6b19a
Aristides Perez <aris.a.perez@gmail.com>
2023-07-20 15:15:18 -0400

    Get top 20 stories to display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+
+  def home
+    # TODO: make sure you handle errors here
+    url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    story_ids = JSON.parse(response)
+
+    @stories = story_ids.first(20).map do |story_id|
+      url = "https://hacker-news.firebaseio.com/v0/item/#{story_id}.json"
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      JSON.parse(response)
+    end
+  end
 end
