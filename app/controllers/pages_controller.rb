98b9ff920717cf10772f4fb401ddeb652f10b757
Aristides Perez <aris.a.perez@gmail.com>
2023-07-21 09:40:00 -0400

    Persist stories and use new story model for view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,11 +7,29 @@ class PagesController < ApplicationController
     response = Net::HTTP.get(uri)
     story_ids = JSON.parse(response)
 
-    @stories = story_ids.first(20).map do |story_id|
-      url = "https://hacker-news.firebaseio.com/v0/item/#{story_id}.json"
-      uri = URI(url)
-      response = Net::HTTP.get(uri)
-      JSON.parse(response)
-    end
+    # TODO: put a caching mechanism here for efficiency if time permits
+
+    non_persisted_stories = story_ids.map(&:to_s) - Story.where(story_id: story_ids).pluck(:story_id)
+    non_persisted_stories.first(10).each { |story_id| create_story(story_id) }
+
+    @stories = Story.where(story_id: story_ids)
+  end
+
+  private
+
+  def create_story(story_id)
+    url = "https://hacker-news.firebaseio.com/v0/item/#{story_id}.json"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    params = create_story_params(JSON.parse(response))
+    Story.create!(params)
+  end
+
+  def create_story_params(res)
+    params = res.slice("by", "id", "score", "time", "title", "url")
+    params["author"] = params.delete("by")
+    params["story_id"] = params.delete("id")
+    params["time"] = Time.at(params.delete("time")).to_datetime
+    params
   end
 end
