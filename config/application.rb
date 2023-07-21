6745a1d10eb6987484e6b3c7715bfd483bf23c8e
Aristides Perez <aris.a.perez@gmail.com>
2023-07-21 08:29:17 -0400

    Remove deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Topnews
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Remove deprecation warning for legacy_connection_handling
+    config.active_record.legacy_connection_handling = false
   end
 end
