3d40591c54157dbc08fb75a0ffc744ca1bdf16df
Neil Daftary <ndaftary@codecademy.com>
2023-08-20 18:46:32 -0400

    Show HN top stories on homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  include FavoritesHelper
+  before_action :authenticate_user! # Force user to sign in before viewing this page.
+
+  def home
+    top_stories_ids = HackernewsClient.get_top_stories
+    @top_stories = top_stories_ids.map do |id|
+      HackernewsClient.get_story(id)
+    end
+  end
 end
