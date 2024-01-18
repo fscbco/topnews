5822b50c13a0d7e2cc9296e30fd8010cf9b0c6f7
David Kitchener <kitchener.david@gmail.com>
2024-01-17 20:47:25 -0500

    update views add test

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ApplicationRecord
- validates :title, presence: true
- validates :url, presence: true
+ validates :title, presence: true, uniqueness: true
+ validates :url, presence: true, uniqueness: true
 
  has_many :user_stories
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe Story, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:url) }
+    it { should validate_uniqueness_of(:title) }
+    it { should validate_uniqueness_of(:url) }
   end
 end
