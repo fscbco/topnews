9f145af1181d975990d5c9add529963279434eba
Melody McClure <melodylayne79@gmail.com>
2024-02-14 17:31:36 -0500

    Implement user sign in, basic page flow, and set up for testing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe User do
   context "creating a new user" do
+
     let(:attrs) do
       { first_name: :foo, last_name: :bar, email: 'f@b.c', password: 'foobar123' }
     end
