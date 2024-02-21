e8a99d91aea38d4ca4406691676e466846ae8a32
Joe Ferrairo <josephferrairo@gmail.com>
2024-02-20 22:09:10 -0500

    Add Likes
    - I couldn't get store_accessor to work so I opted to add methods for
     the raw_data attributes I was calling

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -13,7 +13,7 @@ class FeedController < ApplicationController
     else
       raw_data = HTTParty.get("https://hacker-news.firebaseio.com/v0/item/#{id}.json").to_h
       hacker_news_id = raw_data.dig("id")
-      story = Story.create(hacker_news_id: hacker_news_id, raw_data: raw_data)
+      Story.create(hacker_news_id: hacker_news_id, raw_data: raw_data)
     end
   end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,17 @@
 
 class Story < ApplicationRecord
   serialize :raw_data, Hash
+  has_many :likes
+
+  def title
+    raw_data.dig("title")
+  end
+
+  def by
+    raw_data.dig("by")
+  end
+
+  def url
+    raw_data.dig("url")
+  end
 end
