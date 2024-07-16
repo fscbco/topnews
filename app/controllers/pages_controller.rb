290ca5327f58a00aeab8045c21c1770a164cf148
Jeffrey Martinez <martinezjf2@gmail.com>
2024-07-16 18:39:26 -0400

    used the net/http to retreive data from the json and render it within the view, gave some styling using tailwind

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,19 @@
+
+
+require 'net/http'
+require 'json'
+
 class PagesController < ApplicationController
+
+    # Fetch the data from the api and render it in the home.html.erb if the user is logged in
+    # https://hacker-news.firebaseio.com/v0/item/8863.json
+    def home
+        if user_signed_in?
+      url = URI.parse('https://hacker-news.firebaseio.com/v0/item/8863.json')
+      response = Net::HTTP.get_response(url)
+      @data = JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
+    end 
+    end
+
+
 end
