beeb5b5cfbcc6aa943511531030ce8070a1b906d
Sathyan Mathai <sathyan.mathai@gmail.com>
2024-07-19 21:37:49 -0400

    feat: users can log in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def require_login
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,6 @@
 class PagesController < ApplicationController
+  before_action :require_login, only: [:home]
+  def home
+    @stories = HackerNewsStory.by_popularity
+  end
 end
