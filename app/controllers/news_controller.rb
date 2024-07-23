f10eb359f2584a0d2d0cbdc7631a4ce7191b5a33
Marcus Irven <marcus@marcusirven.com>
2024-07-23 18:36:09 -0400

    setup devise

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,6 @@
 class NewsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     first_top_stories = HackerNews.new.top_stories.first(10)
     @top_stories = first_top_stories.map do |id|
