df114015b345817b4760ae9f2ad32a6f7f064e07
Tony Lum <tonyjlum@gmail.com>
2024-07-22 23:30:24 -0400

    add fav routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  # resource :favorite, only: [:create, :destroy]
+
+  post '/favorite', to: 'favorites#create', as: :favorite_create
+  delete '/favorite', to: 'favorites#destroy', as: :favorite_destroy
+
 end
