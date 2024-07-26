996845e5bf89557e6ca506a05ab9cff75b47750b
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-07-26 17:41:34 -0400

    created association between user, favorites and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts, foreign_key: :post_author_id
+  has_many :favorites
+  has_many :favorited_posts, through: :favorites, source: :post
 end
