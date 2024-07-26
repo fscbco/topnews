1554ca12c11824ba6ae86cb2f0671a53bfcc6f2e
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-07-26 19:35:36 -0400

    refactored UI

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,19 @@
 class FavoritesController < ApplicationController
 
+  def create
+    # finding to verify it exists
+    post = Post.find(params[:post_id])
+    Favorite.create(user: current_user, post:)
+  rescue StandardError => e
+    flash[:alert] = e.message
+  ensure
+    redirect_back(fallback_location: root_path)
+  end
+
   def index
-    @stories = current_user.favorited_posts.paginate(page:, per_page:)
+    @user        = User.find(params[:user_id])
+    @user_faves  = Favorite.where(user_id: current_user.id).pluck(:post_id)
+    @other_faves = Favorite.where.not(user_id: current_user.id).pluck(:post_id)
+    @stories     = @user.favorited_posts.paginate(page:, per_page:)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'stories#index'
   resources :stories
+  resources :favorites, only: [:create, :destroy]
   resources :users, only: [:index] do
     resources :favorites, only: [:index]
   end
