9e12325edfd610d33c58ff1629797980bea43c9e
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-07-26 20:15:06 -0400

    add all action to display all favorites - misc UI refactor

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
 
   def create
-    # finding to verify it exists
+    # finding to verify it exists, so we don't add in garbage data
     post = Post.find(params[:post_id])
     Favorite.create(user: current_user, post:)
   rescue StandardError => e
@@ -15,5 +15,13 @@ class FavoritesController < ApplicationController
     @user_faves  = Favorite.where(user_id: current_user.id).pluck(:post_id)
     @other_faves = Favorite.where.not(user_id: current_user.id).pluck(:post_id)
     @stories     = @user.favorited_posts.paginate(page:, per_page:)
+  end
+
+  def all
+    # Rob Note: By using includes over a join, we can both avoid the N+1 query problem
+    #           and avoid duplicates in the result set.
+    @stories = Post.includes(:post_author, favorites: :user)
+                   .where.not(favorites: { id: nil })
+                   .paginate(page:, per_page:)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'stories#index'
   resources :stories
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy] do
+    get :all, on: :collection
+  end
   resources :users, only: [:index] do
     resources :favorites, only: [:index]
   end
