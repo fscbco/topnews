7a02074b935046e31324f986cc1db09a555a6dee
Marcus Irven <marcus@marcusirven.com>
2024-07-27 09:40:46 -0400

    add ability to create recommendations from the home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'recommendations', to: 'pages#recommendations'
+  resources :recommendations, only: [:create]
 end
