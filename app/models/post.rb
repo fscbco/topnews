6a7dcb84f7f518ac40058f906a5309794a1c3ce2
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-07-26 20:13:40 -0400

    added dependenty destroy to associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :post_author, counter_cache: true
-  enum post_type: [ :story, :job, :comment, :poll, :pollopt ]
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
 
+  enum post_type: [ :story, :job, :comment, :poll, :pollopt ]
   scope :desc_stories, -> { where(post_type: :story).order(id: :desc) }
 end
