4e79b3b9f2c957c75fcf3b9c70564d0aa47e49a3
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-07-28 17:00:41 -0400

    finished specs for creation of a star

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -3,6 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Favorites', type: :request do
+  describe 'POST /create' do
+    let(:user) { create(:user) }
+    let(:db_post) { create(:post) }
+
+    subject(:creation) do
+      post favorites_path(post_id: db_post.id)
+    end
+
+    context 'when user is logged in' do
+      before do
+        sign_in user
+      end
+
+      it 'creates a favorite' do
+        expect { creation }.to change(Favorite, :count).by(1)
+        # falling back to root_path because we don't have a referer set in the test
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'does not create a favorite' do
+        expect { creation }.not_to change(Favorite, :count)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
   describe 'GET /index' do
     let(:user) { create(:user) }
 
