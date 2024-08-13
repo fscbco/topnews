ce8c8d56b44c3213c7443beecf6f84b38125d4c0
John Verdone <jmverdone@gmail.com>
2024-08-13 11:04:47 -0400

    linting

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,12 @@ Rails.application.routes.draw do
   root to: 'news_details#index'
 
   resources :news_details, only: [:index] do
-   collection do 
-    get :liked_index
-   end
+    collection do
+      get :liked_index
+    end
     member do
       patch :upvote
       patch :downvote
-      
     end
   end
-
-
 end
