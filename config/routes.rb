481b38f8f34c7d19db8fab1209f0cb2960a50f36
John Verdone <jmverdone@gmail.com>
2024-08-13 07:25:27 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
+  devise_for :users, only: [:sessions]
+  root to: 'news_details#index'
 
-  resources :news_details do
+  resources :news_details, only: [:index] do
    collection do 
     get :liked_index
    end
